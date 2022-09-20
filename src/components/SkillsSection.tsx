d88918bacb90758a31775b859cdd98db2ea1acf3
Greg Hosking <hoskinggregory@gmail.com>
2022-09-20 14:06:09 -0400

    Add alt text to images in skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -5,17 +5,23 @@ const SkillsSection: React.FunctionComponent = () => {
     <section id='skills-section' className='d-flex align-items-center'>
       <div>
         <h1>SKILLS</h1>
-        <img src={require('../images/aws-icon.png')} />
-        <img src={require('../images/python-icon.png')} />
-        <img src={require('../images/java-icon.png')} />
-        <img src={require('../images/c-plus-plus-icon.png')} />
-        <img src={require('../images/go-icon.png')} />
-        <img src={require('../images/react-icon.png')} />
-        <img src={require('../images/typescript-icon.png')} />
-        <img src={require('../images/javascript-icon.png')} />
-        <img src={require('../images/html-icon.png')} />
-        <img src={require('../images/css-icon.png')} />
-        <img src={require('../images/github-icon.png')} />
+        <img src={require('../images/aws-icon.png')} alt='AWS icon' />
+        <img src={require('../images/python-icon.png')} alt='Python icon' />
+        <img src={require('../images/java-icon.png')} alt='Java icon' />
+        <img src={require('../images/c-plus-plus-icon.png')} alt='C++ icon' />
+        <img src={require('../images/go-icon.png')} alt='Go icon' />
+        <img src={require('../images/react-icon.png')} alt='React icon' />
+        <img
+          src={require('../images/typescript-icon.png')}
+          alt='TypeScript icon'
+        />
+        <img
+          src={require('../images/javascript-icon.png')}
+          alt='JavaScript icon'
+        />
+        <img src={require('../images/html-icon.png')} alt='HTML icon' />
+        <img src={require('../images/css-icon.png')} alt='CSS icon' />
+        <img src={require('../images/github-icon.png')} alt='GitHub icon' />
       </div>
     </section>
   );
