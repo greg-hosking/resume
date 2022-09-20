12725a8355b0b1035a79156c6005f48c0365a07a
Greg Hosking <hoskinggregory@gmail.com>
2022-09-20 13:58:28 -0400

    Complete education section

--- src/components/EducationSection.tsx
+++ src/components/EducationSection.tsx
@@ -5,7 +5,19 @@ const EducationSection: React.FunctionComponent = () => {
     <section id='education-section' className='d-flex align-items-center'>
       <div>
         <h1>EDUCATION</h1>
-        <p>Lorem ipsum dolor sit amet</p>
+        <h3>ENDICOTT COLLEGE</h3>
+        <h5>
+          BACHELOR OF SCIENCE
+          <span>&nbsp;&bull;&nbsp;</span>
+          COMPUTER SCIENCE
+          <span>&nbsp;&bull;&nbsp;</span>
+          <span>SEPTEMBER 2020 - MAY 2023</span>
+        </h5>
+        <ul>
+          <li>Computer Science Club President Fall 2021 to Fall 2022</li>
+          <li>Endicott College Honors Program Member</li>
+          <li>Dean's List Fall 2020 to Spring 2022 (3.84 GPA)</li>
+        </ul>
       </div>
     </section>
   );
