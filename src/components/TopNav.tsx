e7a28e35bc14c704c91d55315d14453d4897b55a
Greg Hosking <hoskinggregory@gmail.com>
2022-09-20 13:56:06 -0400

    Make top nav sticky

--- src/components/TopNav.tsx
+++ src/components/TopNav.tsx
@@ -6,11 +6,11 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const TopNav: React.FunctionComponent = () => {
   return (
-    <Navbar expand='md' variant='dark' sticky='top'>
+    <Navbar expand='lg' variant='dark' className='fixed-top' id='top-nav'>
       <Container fluid>
         <Navbar.Brand href='#about-section'>GREG HOSKING</Navbar.Brand>
-        <Navbar.Toggle aria-controls='top-nav' />
-        <Navbar.Collapse id='top-nav'>
+        <Navbar.Toggle aria-controls='nav' />
+        <Navbar.Collapse id='nav'>
           <Nav className='me-auto'>
             <Nav.Link href='#about-section'>ABOUT</Nav.Link>
             <Nav.Link href='#education-section'>EDUCATION</Nav.Link>
