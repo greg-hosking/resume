f1707f463bd0b213cac93114497fb4f6d204edb3
Greg Hosking <hoskinggregory@gmail.com>
2022-09-20 13:55:56 -0400

    Complete skills section

--- src/components/SkillsSection.tsx
+++ src/components/SkillsSection.tsx
@@ -5,7 +5,17 @@ const SkillsSection: React.FunctionComponent = () => {
     <section id='skills-section' className='d-flex align-items-center'>
       <div>
         <h1>SKILLS</h1>
-        <p>Lorem ipsum dolor sit amet</p>
+        <img src={require('../images/aws-icon.png')} />
+        <img src={require('../images/python-icon.png')} />
+        <img src={require('../images/java-icon.png')} />
+        <img src={require('../images/c-plus-plus-icon.png')} />
+        <img src={require('../images/go-icon.png')} />
+        <img src={require('../images/react-icon.png')} />
+        <img src={require('../images/typescript-icon.png')} />
+        <img src={require('../images/javascript-icon.png')} />
+        <img src={require('../images/html-icon.png')} />
+        <img src={require('../images/css-icon.png')} />
+        <img src={require('../images/github-icon.png')} />
       </div>
     </section>
   );
